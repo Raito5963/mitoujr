dd54c7bf8c87e5a14ffc7ad3fefe045b582720ff
Raito5963 <181489458+Raito5963@users.noreply.github.com>
2025-02-18 14:31:14 +0000

    AppBarを追加し、ページのタイトルを表示。リンクにボタンを追加して、ユーザビリティを向上。

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,19 @@ import {
   Container,
   Paper,
   Typography,
-  Link
+  Link,
+  AppBar
 } from "@mui/material";
 
 export default function Home() {
   return (
     <Container>
-      <Paper elevation={3} sx={{ p: 5 }}>
+      <AppBar sx={{ p: 2, bgcolor:"green"}} >
+        <Typography variant="h2" align="center">
+          Aokiju Labo
+        </Typography>
+      </AppBar>
+      <Paper elevation={3} sx={{ mt:15, p: 5 }}>
         <Typography variant="h2">
           制作物紹介
         </Typography>
@@ -20,9 +26,15 @@ export default function Home() {
         </Typography>
         <br />
         <Typography variant="body1">
-          今回提案するアプリのデモバージョンのフロント側
+          今回提案するアプリのデモバージョンのフロント側。TOMOLプロジェクトにて静岡市長賞、株式会社リバティー賞を受賞。
         </Typography>
         <br />
+        <Link href="https://e55c631a-45d9-4671-7295-38c7f9b35eca.mtayo.net/management" target="_blank" rel="noopener noreferrer">
+          <Button variant="contained" color="primary">
+            FEEDO
+          </Button>
+        </Link>
+        <span> </span>
         <Link href="https://github.com/cube5963/feedo_front" target="_blank" rel="noopener noreferrer">
           <Button variant="contained" color="primary">
             リポジトリを見る
@@ -38,7 +50,7 @@ export default function Home() {
           今回提案するアプリのデモバージョンのバック側
         </Typography>
         <br />
-        <Link href="https://github.com/cube5963/feedo_back"  target="_blank" rel="noopener noreferrer">
+        <Link href="https://github.com/cube5963/feedo_back" target="_blank" rel="noopener noreferrer">
           <Button variant="contained" color="primary">
             リポジトリを見る
           </Button>
@@ -53,7 +65,13 @@ export default function Home() {
           StartUpWeekend静岡7thで制作したサイト。リアルタイムで回答を受け取れるアンケートアプリというコンセプトのもと作成しました。静岡市長奨励賞を獲得。
         </Typography>
         <br />
-        <Link href="https://github.com/cube5963/feedo_old"  target="_blank" rel="noopener noreferrer">
+        <Link href="https://ca55bad4-a542-2735-89ab-670a991e0c24.mtayo.net/" target="_blank" rel="noopener noreferrer">
+          <Button variant="contained" color="primary">
+            FEEDO Old
+          </Button>
+        </Link>
+        <span> </span>
+        <Link href="https://github.com/cube5963/feedo_old" target="_blank" rel="noopener noreferrer">
           <Button variant="contained" color="primary">
             リポジトリを見る
           </Button>
@@ -68,13 +86,13 @@ export default function Home() {
           StartUpWeekend静岡8thで制作したサイト。共通の趣味を持つ人同士が集まれるコミュニティというコンセプトで作成しました。
         </Typography>
         <br />
-        <Link href="https://github.com/Raito5963/SW8th"  target="_blank" rel="noopener noreferrer">
+        <Link href="https://github.com/Raito5963/SW8th" target="_blank" rel="noopener noreferrer">
           <Button variant="contained" color="primary">
             リポジトリを見る
           </Button>
         </Link>
         <span> </span>
-        <Link href="https://sw-8th.vercel.app/"  target="_blank" rel="noopener noreferrer">
+        <Link href="https://sw-8th.vercel.app/" target="_blank" rel="noopener noreferrer">
           <Button variant="contained" color="primary">
             gaga friendsを見る
           </Button>
